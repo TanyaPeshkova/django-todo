cc41ff40b64e038132680f10282876765c7ef393
TanyaPeshkova <peshkova-2002.01@mail.ru>
2022-10-13 02:33:03 +0700

    todo

---
+++ django_todo/api/views.py
@@ -0,0 +1,19 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from base.models import Task
+from .serializers import TaskSerializer
+
+
+@api_view(['GET'])
+def getData(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def addTask(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
